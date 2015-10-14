5f79517cf453dda9776f1fd9cac0f96a410a22ab
Jeff Nelson <dj.synthe@gmail.com>
2015-10-13 21:08:18 -0700

    Adding option to override type in text inputs. Can be used for passwords by adding type="password" to <mdl-text-field>

--- src/angular-material-design-lite.js
+++ src/angular-material-design-lite.js
@@ -18,12 +18,13 @@
   angular.module('mdl').directive('mdlTextField', function(mdlConfig){
     return {
       restrict: 'E',
-      template: '<div class="mdl-textfield mdl-js-textfield" ng-class="ngClass"><input class="mdl-textfield__input" type="text" ng-model="ngModel" /><label class="mdl-textfield__label">{{label}}</label></div>',
+      template: '<div class="mdl-textfield mdl-js-textfield" ng-class="ngClass"><input class="mdl-textfield__input" type="{{type}}" ng-model="ngModel" /><label class="mdl-textfield__label">{{label}}</label></div>',
       scope: {
         ngModel: '='
       },
       link: function($scope, el, $attrs){
         $scope.label = $attrs.label;
+        $scope.type = $attrs.type ? $attrs.type : 'text';
         $scope.ngClass = {
           'mdl-textfield--floating-label': mdlConfig.floating
         };
